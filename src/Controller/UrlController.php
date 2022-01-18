47fa32bbacaab48d0620c501c44b559da0fca47a
Timo <timotheegrollier@gmail.com>
2022-01-18 12:48:27 +0100

    Add logic for url

--- src/Controller/UrlController.php
+++ src/Controller/UrlController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Url;
 use App\Repository\UrlRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +15,17 @@ use Symfony\Component\Validator\Constraints\Url as UrlConstraint;
 
 class UrlController extends AbstractController
 {
+    private $urlRepository;
+
+    public function __construct(UrlRepository $urlRepository)
+    {
+        $this->urlRepository = $urlRepository;
+    }
     /**
      * @Route("/", name="app_home", methods={"GET","POST"})
      * @Route("/", name="app_url_create", methods={"GET","POST"})
      */
-    public function create(Request $request,UrlRepository $urlRepository): Response
+    public function create(Request $request, EntityManagerInterface $em): Response
     {
         $form =  $this->createFormBuilder()
             ->add('original', TextType::class, [
@@ -38,12 +45,20 @@ class UrlController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // dd($request->request->all());
-         $url=  $urlRepository->findOneBy(['original'=>$form->get('original')->getData()]);
+            $url = $this->urlRepository->findOneBy(['original' => $form->get('original')->getData()]);
 
-         if ($url){
-           return $this->redirectToRoute('app_url_preview',['shortened'=>$url->getShortened()]);
-         }
+            if ($url) {
+                return $this->redirectToRoute('app_url_preview', ['shortened' => $url->getShortened()]);
+            }
 
+            $url = new Url;
+            $url->setOriginal($form['original']->getData());
+            $url->setShortened($this->getUniqueShortenedString());
+            $em->persist($url);
+            $em->flush();
+
+
+            return $this->render('url/preview.html.twig', compact('url'));
         }
 
 
@@ -64,9 +79,20 @@ class UrlController extends AbstractController
      * @Route("/{shortened}",name="app_url_show", methods="GET")
      */
 
-    public function show(Url $url):Response
+    public function show(Url $url): Response
     {
         return $this->redirect($url->getOriginal());
     }
 
+
+    private function getUniqueShortenedString(): string
+    {
+        $shortened = substr(bin2hex(random_bytes(32)), 0, 6);
+
+        if ($this->urlRepository->findOneBy(['shortened' => $shortened])) {
+            return $this->getUniqueShortenedString();
+        }
+
+        return $shortened;
+    }
 }
